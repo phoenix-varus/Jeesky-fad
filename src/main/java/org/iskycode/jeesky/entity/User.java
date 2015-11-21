fd115b3689d7082e6b4f6f0183202b73cdf07d09
phoenix <phoenix@192.168.1.102>
2015-11-22 01:30:01 +0800

    框架：代码合并。

--- src/main/java/org/iskycode/jeesky/entity/User.java
+++ src/main/java/org/iskycode/jeesky/entity/User.java
@@ -3,6 +3,7 @@ package org.iskycode.jeesky.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -11,6 +12,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class User {
 	private String id;
 	private String name;
+	private String city_id;
+	private String type;
 
 	@Id
 	@GeneratedValue
@@ -29,6 +32,23 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@JoinColumn(name = "city_id")
+	public String getCity_id() {
+		return city_id;
+	}
+
+	public void setCity_id(String city_id) {
+		this.city_id = city_id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
